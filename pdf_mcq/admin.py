3f786e59805f5832a848f6545614c6307dbec8b5
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2026-04-04 14:52:21 +0530

    Nothing happened

--- pdf_mcq/admin.py
+++ pdf_mcq/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+    # from .models import MCQQuestion,MCQTest
 
-# Register your models here.
+    # # Register your models here.
+
+    # class MCQQuestionInline(admin.TabularInline):
+    #     model = MCQQuestion
+    #     extra =0
+        
+    # @admin.register(MCQTest)
+    # class MCQTestAdmin(admin.ModelAdmin):
+    #     list_display=("mcq_count","created_at")
+    #     inlines=[MCQQuestionInline]
+        
+        
+    # @admin.register(MCQQuestion)
+    # class MCQQuestionAdmin(admin.ModelAdmin):
+    #     list_display=("question_text","correct_answer")
+        
--- pdf_mcq/models.py
+++ pdf_mcq/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# class MCQTest(models.Model):
+#     mcq_count = models.IntegerField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+    
+#     def __str__(self):
+#         return f"({self.mcq_count})"
+    
+    
+# class MCQQuestion(models.Model):
+#     test= models.ForeignKey(MCQTest,on_delete=models.CASCADE,related_name='questions')
+    
+#     question_text = models.TextField()
+#     option_a = models.TextField()
+#     option_b = models.TextField()
+#     option_c = models.TextField()
+#     option_d = models.TextField()
+    
+#     correct_answer = models.CharField(max_length=1)
+#     explanation= models.TextChoices()
+    
+#     def __str__(self):
+#         return self.question_text[:50]
