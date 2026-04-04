f7330dbdb703fdf69fbfb8117c2ee19b894ea593
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2026-04-05 00:58:23 +0530

    Add database for mcq generations

--- pdf_mcq/admin.py
+++ pdf_mcq/admin.py
@@ -1,19 +1,36 @@
 from django.contrib import admin
-    # from .models import MCQQuestion,MCQTest
+from .models import PDFDocument, MCQSession, MCQQuestion, UserAnswer
 
-    # # Register your models here.
+class PDFDocumentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'file_name', 'file_size', 'uploaded_at')
+    list_filter = ('user', 'uploaded_at')
+    search_fields = ('file_name', 'user__username')
+    readonly_fields = ('uploaded_at',)
 
-    # class MCQQuestionInline(admin.TabularInline):
-    #     model = MCQQuestion
-    #     extra =0
-        
-    # @admin.register(MCQTest)
-    # class MCQTestAdmin(admin.ModelAdmin):
-    #     list_display=("mcq_count","created_at")
-    #     inlines=[MCQQuestionInline]
-        
-        
-    # @admin.register(MCQQuestion)
-    # class MCQQuestionAdmin(admin.ModelAdmin):
-    #     list_display=("question_text","correct_answer")
-        
+class MCQSessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'session_id', 'user', 'mcq_count', 'created_at')
+    list_filter = ('user', 'created_at')
+    search_fields = ('session_id', 'user__username')
+    readonly_fields = ('created_at',)
+
+class MCQQuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_number', 'user', 'session', 'question_text_short', 'correct_answer', 'created_at')
+    list_filter = ('user', 'session', 'correct_answer', 'created_at')
+    search_fields = ('question_text', 'user__username')
+    readonly_fields = ('created_at',)
+    
+    def question_text_short(self, obj):
+        return obj.question_text[:50] + '...' if len(obj.question_text) > 50 else obj.question_text
+    question_text_short.short_description = 'Question'
+
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'question', 'session', 'selected_answer', 'is_correct', 'answered_at')
+    list_filter = ('user', 'is_correct', 'answered_at')
+    search_fields = ('user__username', 'question__question_text')
+    readonly_fields = ('answered_at',)
+
+# Register all models
+admin.site.register(PDFDocument, PDFDocumentAdmin)
+admin.site.register(MCQSession, MCQSessionAdmin)
+admin.site.register(MCQQuestion, MCQQuestionAdmin)
+admin.site.register(UserAnswer, UserAnswerAdmin)
